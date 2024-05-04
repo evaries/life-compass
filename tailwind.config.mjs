82ac81f5c54f3e6cf4978438cececde0349ad36a
evaries <nadtochii.e@gmail.com>
2024-05-04 12:23:43 +0800

    mobile view

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,24 +1,23 @@
 import { fontFamily } from 'tailwindcss/defaultTheme'
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
-    extend: {
-      colors: {
-        bgColor: 'var(--theme-bg)',
-        textColor: 'var(--theme-text)',
-        link: 'var(--theme-link)',
-        accent: 'var(--theme-accent)',
-        'accent-2': 'var(--theme-accent-2)'
-      },
-      fontFamily: {
-        // Add any custom fonts here
-        sans: ['Inknut Antiqua', ...fontFamily.sans],
-        serif: [...fontFamily.serif]
-      },
-      transitionProperty: {
-        height: 'height'
-      }
-    }
-  },
-  plugins: []
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				bgColor: 'var(--theme-bg)',
+				textColor: 'var(--theme-text)',
+				link: 'var(--theme-link)',
+				accent: 'var(--theme-accent)',
+				'accent-2': 'var(--theme-accent-2)'
+			},
+			fontFamily: {
+				sans: ['Inknut Antiqua', ...fontFamily.sans],
+				serif: [...fontFamily.serif]
+			},
+			transitionProperty: {
+				height: 'height'
+			}
+		}
+	},
+	plugins: []
 }
